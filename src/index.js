b068dded1b74aeb39122e7e89c4d5d3a5f23d1d2
Alan Russell <ajrussellaudio@gmail.com>
2018-11-14 16:55:27 +0000

    add todos

--- src/index.js
+++ src/index.js
@@ -12,23 +12,28 @@ const makeButton = () => {
 
 const sendMsg = msg => console.log("Sending:", msg);
 
-const playNote$ = pipe(
-  map(([note, velocity]) => new NoteOn(note, velocity)),
-  tap(note => sendMsg(note)),
-  delay(1000),
-  map(note => new NoteOff(note.number)),
-  tap(noteOff => sendMsg(noteOff))
-);
+const playNote = (dur, tempo) =>
+  pipe(
+    // TODO: abstract this out
+    // TODO: how the fuck does tempo/dur work?
+    map(([note, velocity]) => new NoteOn(note, velocity)),
+    tap(note => sendMsg(note)),
+    delay(1000),
+    map(note => new NoteOff(note.number)),
+    tap(noteOff => sendMsg(noteOff))
+  );
 
-const scale = [60, 63, 65, 68, 70, 72];
+const tempo = bpm => (60 / bpm) * 1000;
 
 const app = () => {
   // const click$ = fromEvent(makeButton(), "click");
-  const click$ = timer(0, 1500);
+  const scale = [60, 63, 65, 68, 70, 72];
+
+  const click$ = timer(0, tempo(128));
   const noteNumber$ = click$.pipe(map(() => choose(scale)));
   const velocity$ = click$.pipe(map(() => int(random(128))));
   zip(noteNumber$, velocity$)
-    .pipe(playNote$)
+    .pipe(playNote(4, tempo(128)))
     .subscribe();
 };
 
